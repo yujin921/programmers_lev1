b513cf31431dc8a89508ee2c11803ace01946c20
yujin921 <tlsdwls56@gmil.com>
2025-06-04 11:10:12 +0900

    day02 문제 풀이 5개

---
+++ day01/src/level01/day02/Solution02.java
@@ -0,0 +1,8 @@
+package level01.day02;
+class Solution {
+    public String[] solution(String[] strings, int n) {
+        String[] answer = {};
+        return answer;
+    }
+}
+
---
+++ day01/src/level01/day02/Solution05.java
@@ -0,0 +1,9 @@
+package level01.day02;
+
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+        return answer;
+    }
+}
+
